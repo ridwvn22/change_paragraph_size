05326e9806baebd7cfaeb5fe3b15d691454c8411
Ridwan Dahir <ridwandahir@Ridwans-MacBook-Air-2.local>
2023-10-10 08:43:45 -0500

    2.1.9_jQuery_paragraph_size

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log("minimizing font");
+$(document).ready(function() {
+    //console.log("ready");
+    $("button").click("minimize", function(){
+    $("footer").css("fontSize", "7px" );
+    $("footnote").css("fontSize", "10px" );
+    });  
+}); 
